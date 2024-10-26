0b84da7d7894dea27ccbcdf68553881ce4024d8c
wajih8 <79630987+wajih8@users.noreply.github.com>
2024-10-26 01:26:47 +0100

    Delete api_server directory

--- api_server/test.js
+++
@@ -1,9 +0,0 @@
-const spaw = require("child_process").spawn;
-
-const data = ["wa basas"];
-const pys = spaw("python", ["./hey.py"]);
-pys.stdout.on("data", (data) => {
-  x = JSON.parse(data.toString());
-
-  console.log(x.wa);
-});
